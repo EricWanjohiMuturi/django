7184a3cabaa280f934c26ccb9b892f7ffc8ef4ad
emuturi124@gmail.com <emuuturi124@gmail.com>
2025-04-28 21:52:35 +0300

    initialized the project, coming soon page

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'blogs',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +61,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
